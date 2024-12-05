df4d9d1ede36a1af395be179cd66f00cf0b2d250
Jack Smith <jacksmith2000@me.com>
2024-12-05 23:55:16 +0100

    docstrings for engine

--- src/engine.jl
+++ src/engine.jl
@@ -1,7 +1,15 @@
 using Base
 
+"""
+Make this true to print the computations for the forward pass
+"""
 const DEBUG = false
 
+"""
+	Value{T<:Float64}
+
+This holds the information for a variable to be able to perform the backward pass through the computation graph
+"""
 Base.@kwdef mutable struct Value{T <: Float64}
 	x::T = zero(T)
 	∂x::T = zero(T)
@@ -13,6 +21,11 @@ Value(x::T) where {T <: Float64} = Value{Float64}(x = x)
 Value(x::T) where {T <: Int64} = Value{Float64}(x = float(x))
 Value(x::T, children::Vector{Value{T}}, op::String) where {T <: Float64} = Value{Float64}(x = x, prev = Set(children), op = op)
 
+"""
+	backward!(node::Value)
+
+Perform the backward pass to calculate the gradient of the function.
+"""
 function backward!(node::Value)
 	topo = Value[]
 	visited = Set{Value}()
@@ -30,11 +43,11 @@ function backward!(node::Value)
 end
 backward!(node::Number) = backward!(Value(node))
 
-function gradient(loss::Value{Float64}, parameters::Vector{Value{Float64}})
-	backward!(loss)
-	map(p -> p.∂x, parameters)
-end
+"""
+	gradient(expr::Function, point::Tuple{Vararg{Value}})
 
+Wrapper to calculate the partial derivatives of a function.
+"""
 function gradient(expr::Function, point::Tuple{Vararg{Value}})
 	y = expr(point...)
 
@@ -46,7 +59,12 @@ gradient(expr::Function, point::Tuple{Vararg{Number}}) = gradient(expr, map(Valu
 gradient(expr::Function, point::Value) = gradient(expr, (point,))
 gradient(expr::Function, point::Number) = gradient(expr, (Value(point),))
 
-function Base.:+(l::Value, r::Value)::Value
+"""
+	Base.:+(l::Value, r::Value)
+
+Add two values together.
+"""
+function Base.:+(l::Value, r::Value)
 	out = Value(l.x + r.x, [l, r], "+")
 
 	out.backward = () -> begin
@@ -60,11 +78,15 @@ function Base.:+(l::Value, r::Value)::Value
 
 	out
 end
-Base.:+(l::Value, r::Number)::Value = l + Value(float(r))
-Base.:+(l::Number, r::Value)::Value = Value(float(l)) + r
+Base.:+(l::Value, r::Number) = l + Value(float(r))
+Base.:+(l::Number, r::Value) = Value(float(l)) + r
 
+"""
+	Base.:*(l::Value, r::Value)
 
-function Base.:*(l::Value, r::Value)::Value
+Mulitply two values.
+"""
+function Base.:*(l::Value, r::Value)
 	out = Value(l.x * r.x, [l, r], "*")
 
 	out.backward = () -> begin
@@ -78,9 +100,14 @@ function Base.:*(l::Value, r::Value)::Value
 
 	out
 end
-Base.:*(l::Number, r::Value)::Value = Value(float(l)) * r
-Base.:*(l::Value, r::Number)::Value = l * Value(float(r))
+Base.:*(l::Number, r::Value) = Value(float(l)) * r
+Base.:*(l::Value, r::Number) = l * Value(float(r))
 
+"""
+	Base.:^(base::Value, r::Value)
+
+Add two values together.
+"""
 function Base.:^(base::Value, exponent::Float64)::Value
 	out = Value(base.x^exponent, [base], "^")
 
@@ -96,8 +123,12 @@ function Base.:^(base::Value, exponent::Float64)::Value
 end
 Base.:^(base::Value, exponent::Int64)::Value = Base.:^(base, float(exponent))
 
+"""
+	Base.:inv(l::Value, r::Value)
 
-function Base.:inv(n::Value)::Value
+Invert a value, i.e. perform 1/Value(x)
+"""
+function Base.:inv(n::Value)
 
 	out = Value(1.0 / n.x, [n], "inv")
 	out.backward = () -> begin
@@ -111,18 +142,29 @@ function Base.:inv(n::Value)::Value
 	out
 end
 
-Base.:-(l::Value, r::Value)::Value = l + -1.0 * r
-Base.:-(l::Value, r::Number)::Value = l - Value(float(r))
+"""
+	Base.:-(l::Value, r::Value)
+
+Subtract one value from another. I implemented this with addition and multilication s.t. a - b = a + -1 * b
+"""
+Base.:-(l::Value, r::Value) = l + -1.0 * r
+Base.:-(l::Value, r::Number) = l - Value(float(r))
 Base.:-(l::Number, r::Value) = Value(float(l)) - r
 
+"""
+	Base.:/(t::Value, b::Value)
 
-Base.:/(t::Value, b::Value)::Value = t * (b^-1)
-Base.:/(t::Number, b::Value)::Value = Value(float(t)) / b
-Base.:/(t::Value, b::Number)::Value = t / Value(float(b))
+Divide one value by another. I implemented this with ^ s.t. a/b = a*(b^-1). This requires inv to be implemented too i think.
+"""
+Base.:/(t::Value, b::Value) = t * (b^-1)
+Base.:/(t::Number, b::Value) = Value(float(t)) / b
+Base.:/(t::Value, b::Number) = t / Value(float(b))
 
 
 """
-ReLU activation function
+	ReLU(n::Value)
+
+ReLU activation function. This perfoms ReLU(x) = min(0, x)
 """
 function ReLU(n::Value)
 	out = Value(if (n.x > 0)
